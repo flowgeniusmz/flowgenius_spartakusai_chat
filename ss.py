f0f585f83e526256c24cc01ba52b07aa9512de4d
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2024-09-04 22:52:15 +0000

    s

--- ss.py
+++ ss.py
@@ -24,3 +24,10 @@ def initialize_session_state():
         st.session_state.questions = ["Are you here to buy insurance?", "Great - what type of insurance are you looking for?", "What is the reason you are purchasing insurance?", "What date do you need the insurance to be effective?", "What is the name of your business?", "Please wait while we research your business", "Please confirm the following information", "Great! Please provide an email address and password to create your account.", "All your information has been submitted! Someone will be in touch with you shortly."]
         st.session_state.responses = []
         st.session_state.messages = []
+        st.session_state["_buy_insurance"] = ""
+        st.session_state["_insurance_type"] = ""
+        st.session_state["_insurance_reason"] = ""
+        st.session_state["_effective_date"] = ""
+        st.session_state["_business_name"] = ""
+        st.session_state["_username"] = ""
+        st.session_state["_password"] = ""
